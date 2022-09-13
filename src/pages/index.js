ca5a5c4c10076ed71720fea61bc71dc1a0b6405d
chai2010 <chaishushan@gmail.com>
2022-09-13 12:22:28 +0800

    home page support i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,23 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Translate, {translate} from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const siteConfig_title = translate({id: 'homepage.title', "message": "KusionStack"});
+  const siteConfig_tagline = translate({id: 'homepage.tagline', "message": "一站式可编程配置技术栈"});
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">{siteConfig_title}</h1>
+        <p className="hero__subtitle">{siteConfig_tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/user_docs/getting-started/usecase">
-            快速开始 - 15min ⏱️
+            <Translate>Quick Start - 15min ⏱️</Translate>
           </Link>
         </div>
       </div>
@@ -27,10 +31,13 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+  const siteConfig_title = translate({id: 'homepage.title', "message": "KusionStack"});
+  const description = translate({"message":"Description will go into a meta tag in <head />"});
+
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig_title}`}
+      description={description}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
