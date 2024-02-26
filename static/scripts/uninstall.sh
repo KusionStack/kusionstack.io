5e81ef9f903dc70bc94c4c1a741a5b8dfbe88273
healthjyk <healthjyk@163.com>
2024-02-26 16:13:24 +0800

    feat: add windows-usable install.ps1 and uninstall.ps1

--- static/scripts/uninstall.sh
+++ static/scripts/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ------------------------------------------------------------
-# Copyright 2022 The KusionStack Authors
+# Copyright 2024 The KusionStack Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
