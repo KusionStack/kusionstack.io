e609fb44172e856618a4ec16fe715929640558d9
elliotxx <951376975@qq.com>
2024-05-30 20:12:52 +0800

    feat: support independent locale dropdown for sub product, add zh to config

--- src/theme/NavbarItem.js
+++ src/theme/NavbarItem.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import OriginalNavBarItem from '@theme-original/NavbarItem';
-import { useLocation }  from '@docusaurus/router';
+import React from "react";
+import OriginalNavBarItem from "@theme-original/NavbarItem";
+import { useLocation } from "@docusaurus/router";
 
 export default function NavbarItem(props) {
-	const { docsPluginId, type } = props
-	const { pathname } = useLocation()
+  const { docsPluginId, type } = props;
+  const { pathname } = useLocation();
 
-	if (type === 'docsVersionDropdown' && pathname.search(new RegExp(`^/${docsPluginId}/`, 'g')) === -1) {
-		return (<></>)
-	}
+  if (
+    type === "docsVersionDropdown" &&
+    pathname.search(new RegExp(`^/${docsPluginId}/`, "g")) === -1 &&
+    !pathname?.includes(docsPluginId)
+  ) {
+    return <></>;
+  }
 
-	return (
-		<>
-			<OriginalNavBarItem {...props} />
-		</>
-	);
+  if (type === "localeDropdown" && !pathname?.includes("karpor")) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <OriginalNavBarItem {...props} />
+    </>
+  );
 }
